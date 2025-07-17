7af27da211c98452a2c14fbbf35697b3c3b4bbae
ashimmaity81 <maityashim81@gmail.com>
2025-07-18 00:56:00 +0530

    profile updation complete

--- api/v1/users/controllers.js
+++ api/v1/users/controllers.js
@@ -1,13 +1,56 @@
-const sendUserBasicInfoController = (req, res) => {
-    const userInfo = req.user;
+const { UserModel } = require("../../../models/userSchema");
+const { handleGenericAPIError } = require("../../../utils/controllerHelpers");
 
-    res.status(200).json({
-        isSuccess: true,
-        message: "User is valid!",
-        data: {
-            user: userInfo,
-        },
-    });
+const sendUserBasicInfoController = (req, res) => {
+  const userInfo = req.user;
+
+  res.status(200).json({
+    isSuccess: true,
+    message: "User is valid!",
+    data: {
+      user: userInfo,
+    },
+  });
 };
 
-module.exports = { sendUserBasicInfoController };
+const sendUserDetailsController = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const user = await UserModel.findById(_id).select("-password");
+    res.status(200).json({
+      isSuccess: true,
+      message: "User details found!",
+      data: {
+        user: user,
+      },
+    });
+  } catch (err) {
+    handleGenericAPIError("sendUserDetailsController", req, res, err);
+  }
+};
+const updateUserDetails = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { name, gender } = req.body;
+    const updateUser = await UserModel.findByIdAndUpdate(
+      _id,
+      { name, gender },
+      { new: true }
+    ).select("-password");
+    res.status(200).json({
+      isSuccess: true,
+      message: "Profile Updated",
+      data: {
+        user: updateUser,
+      },
+    });
+  } catch (err) {
+    handleGenericAPIError("sendUserDetailsController", req, res, err);
+  }
+};
+
+module.exports = {
+  sendUserBasicInfoController,
+  sendUserDetailsController,
+  updateUserDetails,
+};
